5be8c782ecd84227f3e6dc806517d2eff6d8a197
Eric Marrone <emarrone17@gmail.com>
2018-01-03 15:19:39 -0500

    Finish adding ajax query

--- election.js
+++ election.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'JSON'
+  }).done(function(responseData){
+    var candidates = document.querySelector('#list');
+    responseData.candidates.forEach(function(candidate){
+      var li = document.createElement('li');
+      li.innerHTML =  `<p>Name: ${candidate.name}<br>Votes: ${candidate.votes}</p>`
+      list.append(li)
+    })
+  })
 
 });
